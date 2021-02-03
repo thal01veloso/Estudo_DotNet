ac9e73d338f5aa243ca32f9afeb81a190056e32d
tsvelosoIblueConsulting <tsvelosoIblue@iblueconsulting.com.br>
2021-02-03 13:42:45 -0300

    Criado uma classe gerenciadora

---
+++ LacosEinteracoes/BuscasPorFiltro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LacosEinteracoes
+{
+    public class BuscasPorFiltro
+    {
+        public string Buscarcomforeach(List<string> lista, string filtro)
+        {
+            foreach (string item in lista)
+            {
+                if (item.Equals(filtro))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        //2ª forma de visualizar uma lista com linq
+        public string Buscarcomlinq(List<string> lista, string filtro)
+        {
+            return (from item in lista where item.Equals(filtro) select item).First();
+        }
+        //3ª forma de visualizar uma lista com lambda
+        public string Buscarcomlinqlambda(List<string> lista, string filtro)
+        {
+            return lista.First(x => x.Equals(filtro));
+        }
+
+        //4ª forma de visualizar uma lista com linklambda que contém
+        public List<string> BuscarListacomlinq(List<string> lista, string filtro)
+        {
+            return (from item in lista where item.Contains(filtro) select item).ToList();
+        }
+
+        //5ª forma de visualizar uma lista com lambda que contém
+        public List<string> BuscarListacomlinqlambda(List<string> lista, string filtro)
+        {
+            return (from item in lista where item.Contains(filtro) select item).ToList();
+        }
+    }
+}
--- LacosEinteracoes/Program.cs
+++ LacosEinteracoes/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 
 namespace LacosEinteracoes
 {
@@ -16,36 +16,11 @@ namespace LacosEinteracoes
             sacolao.Add("Brocolis");
             sacolao.Add("Limão");
 
-            BuscarListacomlinq(sacolao, "l").ForEach(x => Console.WriteLine(x));
-            //1ª forma de visualizar uma lista com foreach
-            static string Buscarcomforeach(List<string> lista, string filtro)
-            {
-                foreach (string item in lista)
-                {
-                    if (item.Equals(filtro))
-                    {
-                        return item;
-                    }
-                }
-                return null;
-            }
-            //2ª forma de visualizar uma lista com linq
-            static string Buscarcomlinq(List<string> lista, string filtro)
-            {
-                return (from item in lista where item.Equals(filtro) select item).First();
-            }
-            //3ª forma de visualizar uma lista com lambda
-            static string Buscarcomlinqlambda(List<string> lista, string filtro)
-            {
-                return lista.First(x => x.Equals(filtro));
-            }
 
-            //4ª forma de visualizar uma lista com lambda que contém
-            static List<string> BuscarListacomlinq(List<string> lista, string filtro)
-            {
-                return (from item in lista where item.Contains(filtro) select item).ToList();
-            }
-            
+            BuscasPorFiltro buscas = new BuscasPorFiltro();
+            buscas.BuscarListacomlinqlambda(sacolao, "l").ForEach(x => Console.WriteLine(x));
+            //1ª forma de visualizar uma lista com foreach
+
         }
 
     }
