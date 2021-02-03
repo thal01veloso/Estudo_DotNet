456c9425e5806087697e3cd9c9cb513883e1d0f2
tsvelosoIblueConsulting <tsvelosoIblue@iblueconsulting.com.br>
2021-02-03 10:21:52 -0300

    Primeiro commit-Estudos C#/Dotnet

---
+++ LacosEinteracoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LacosEinteracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sacolao = new List<string>();
+            sacolao.Add("Alface");
+            sacolao.Add("Tomate");
+            sacolao.Add("Laranja");
+            sacolao.Add("Couve");
+            sacolao.Add("Brocolis");
+            //1ª forma de visualizar uma lista
+            foreach (var item in sacolao)
+            {
+                Console.WriteLine(item);
+            };
+
+
+
+        }
+    }
+
+}
