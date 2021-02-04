a8a19dc6135adb62540d18cbdb82859c5431db87
tsvelosoIblueConsulting <tsvelosoIblue@iblueconsulting.com.br>
2021-02-04 15:55:40 -0300

    Dicionários e interações

--- LacosEinteracoes/Program.cs
+++ LacosEinteracoes/Program.cs
@@ -15,10 +15,35 @@ namespace LacosEinteracoes
             sacolao.Add("Couve");
             sacolao.Add("Brocolis");
             sacolao.Add("Limão");
+            sacolao.Add("Abacate");
+            sacolao.Add("Couve-flor");
+
+            Dictionary<int, string> tabela = new Dictionary<int, string>();
+            tabela.Add(10, "tomates");
+            tabela.Add(12, "laranjas");
+            tabela.Add(8, "abacate");
+            tabela.Add(15, "bananas");
+            tabela.Add(9, "melancia");
+            tabela.Add(20, "jabuticabas");
+            tabela.Add(17, "limão");
+
+            var total = 0;
+            foreach (var item in tabela)
+            {
+                total = total + item.Key;
+            }
+            tabela.Add(total, "Total");
+            
+            foreach(var item in tabela)
+            {
+                Console.WriteLine(item);
+            }
 
 
-            BuscasPorFiltro buscas = new BuscasPorFiltro();
-            buscas.BuscarListacomlinqlambda(sacolao, "l").ForEach(x => Console.WriteLine(x));
+
+
+            //BuscasPorFiltro buscas = new BuscasPorFiltro();
+            //buscas.BuscarListacomlinqlambda(sacolao, "a").ForEach(x => Console.WriteLine(x));
             //1ª forma de visualizar uma lista com foreach
 
         }
